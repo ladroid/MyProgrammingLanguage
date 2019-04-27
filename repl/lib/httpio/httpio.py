e2c00611c1aab4af453677e7b7df16587baf9a9a
ladroid <puatachok@gmail.com>
2019-04-27 17:28:52 +0300

    improve httpio lib

--- repl/lib/httpio/httpio.py
+++ repl/lib/httpio/httpio.py
@@ -1,10 +1,48 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+class SimpleHTMLMaker:
+    def make_dir(self, path):
+        pathing = path + "html/"
+        try:
+            os.makedirs(os.path.dirname(pathing))
+        except OSError:
+            print ("Creation of the directory %s failed" % pathing)
+        else:  
+            print ("Successfully created the directory %s " % pathing)
+
+    def make(self):
+        st = os.path.realpath('./html/index.html')
+        rootdir = st
+        print(rootdir)
+        print(type(rootdir))
+        with open(rootdir) as f:
+            return f.read()
 
 class SimpleHTTPRequest(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
+    html_maker = SimpleHTMLMaker()
 
-httpstart = HTTPServer(('localhost', 8000), SimpleHTTPRequest)
-httpstart.serve_forever()
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(bytes(self.html_maker.make(), "utf-8"))
+
+    def do_POST(self):
+        self._set_headers()
+        self.wfile.write(b"<html><body><h1>POST!</h1></body></html>")
+
+def run():
+    try:
+        print("Start server...")
+        httpstart = HTTPServer(('localhost', 8000), SimpleHTTPRequest)
+        httpstart.serve_forever()
+    except KeyboardInterrupt:
+        print("\nShut down server...")
+        httpstart.socket.close()
